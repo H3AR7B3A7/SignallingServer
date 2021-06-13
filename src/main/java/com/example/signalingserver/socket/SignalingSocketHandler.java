203290fdbac9f70755bbab3a9b55e0416d9d2b0b
H3AR7B3A7 <steven.d.hondt.sdh@gmail.com>
2021-06-13 20:18:31 +0200

    :sparkles: Handling hangup and loss of connection

--- src/main/java/com/example/signalingserver/socket/SignalingSocketHandler.java
+++ src/main/java/com/example/signalingserver/socket/SignalingSocketHandler.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
@@ -15,49 +16,44 @@ import java.util.Set;
 
 @Service
 public class SignalingSocketHandler extends TextWebSocketHandler {
+    private final Set<WebSocketSession> sessions = new HashSet<>();
+
     private static final Logger LOG = LoggerFactory.getLogger(SignalingSocketHandler.class);
-
-    private static final String OFFER_TYPE = "offer";
-    private static final String ANSWER_TYPE = "answer";
-    private static final String HANGUP_TYPE = "hangup";
-    private static final String ICE_TYPE = "ice-candidate";
-
     private static final ObjectMapper objectMapper = new ObjectMapper();
 
-    private Set<WebSocketSession> sessions = new HashSet<>();
+    private static final String HANGUP_TYPE = "hangup";
 
     @Override
-    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
-        LOG.debug("handleTextMessage : {}", message.getPayload());
-
-        SignalMessage signalMessage = objectMapper.readValue(message.getPayload(), SignalMessage.class);
-        System.out.println(signalMessage.toString());
-
-        publishToOtherClients(session, message);
-
-//        if (OFFER_TYPE.equalsIgnoreCase(signalMessage.getType())) {
-//
-//
-//
-//        } else if (ICE_TYPE.equalsIgnoreCase(signalMessage.getType())){
-////            System.out.println(signalMessage.getData());
-//
-//            if (this.session == null || !this.session.isOpen()) {
-//                this.session = session;
-//            } else {
-//                LOG.debug("Client already in use...");
-//            }
-//            this.session.sendMessage(new TextMessage(resendingMessage));
-//        } else if (HANGUP_TYPE.equalsIgnoreCase(signalMessage.getType())){
-//            this.session = null;
-//        }
+    public void afterConnectionEstablished(WebSocketSession session) {
+        LOG.info("*** Connection established with {} ***", session.getId());
+        sessions.add(session);
     }
 
-    private void publishToOtherClients(WebSocketSession session, TextMessage message) {
+    @Override
+    protected void handleTextMessage(WebSocketSession session, TextMessage message) {
+        LOG.info("*** Handling TextMessage : {} ***", message.getPayload());
+        publishMessageToOtherClients(session, message);
+    }
+
+    @Override
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) {
+        LOG.info("*** Connection error {} with reason {} ***", session.getId(), status.getReason());
+        sessions.remove(session);
+        sendHangUpMessageToOtherSessions();
+    }
+
+    @Override
+    public void handleTransportError(WebSocketSession session, Throwable exception) {
+        LOG.info("*** Connection error {} with status: {} ***", session.getId(), exception.getLocalizedMessage());
+        sessions.remove(session);
+        sendHangUpMessageToOtherSessions();
+    }
+
+    private void publishMessageToOtherClients(WebSocketSession session, TextMessage message) {
 
         sessions.stream().filter(s -> !s.equals(session)).forEach(s -> {
             if (s.isOpen()) {
-                LOG.warn(session.getId() + " publishes to " + s.getId());
+                LOG.info("*** {} published a message to {} ***", session.getId(), s.getId());
                 try {
                     s.sendMessage(message);
                 } catch (IOException e) {
@@ -67,11 +63,17 @@ public class SignalingSocketHandler extends TextWebSocketHandler {
         });
     }
 
-    @Override
-    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
-        LOG.info("[" + session.getId() + "] Connection established " + session.getId());
+    private void sendHangUpMessageToOtherSessions() {
+        final SignalMessage message = new SignalMessage();
+        message.setType(HANGUP_TYPE);
 
-        sessions.add(session);
+        sessions.forEach(webSocket -> {
+            try {
+                webSocket.sendMessage(new TextMessage(getString(message)));
+            } catch (Exception e) {
+                LOG.warn("!!! Error while sending message. !!!", e);
+            }
+        });
     }
 
     private static String getString(final SignalMessage message) throws Exception {
